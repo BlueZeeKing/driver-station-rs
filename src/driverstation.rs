5cd1419b60c38116c432ca213b330b9a71931a26
BlueZeeKing <github@blueish.dev>
2023-06-10 14:02:16 -0400

    Add `TeamNumber` struct

--- src/driverstation.rs
+++ src/driverstation.rs
@@ -1,10 +1,21 @@
-use std::borrow::ToOwned;
+use std::net::Ipv4Addr;
 use tokio::net::UdpSocket;
-use tokio::task::{self, JoinHandle, AbortHandle};
+use tokio::task::AbortHandle;
 use tokio::time::{self, Duration, Interval};
 
+#[derive(Clone, Copy)]
+pub struct TeamNumber(u16);
+
+impl TeamNumber {
+    pub fn new(number: u16) -> Self {
+        assert!(number > 0 && number <= 9999);
+
+        Self(number)
+    }
+}
+
 pub struct DriverStation {
-    team_number: u16,
+    team_number: TeamNumber,
     socket: Option<UdpSocket>,
     quit: bool,
     count: u16,
@@ -15,7 +26,7 @@ pub struct DriverStation {
 impl Default for DriverStation {
     fn default() -> Self {
         Self {
-            team_number: 0,
+            team_number: 0.into(),
             socket: None,
             quit: false,
             count: 0,
@@ -28,44 +39,57 @@ impl Default for DriverStation {
 impl DriverStation {
     pub fn new(team: u16) -> Self {
         let mut ds = Self::default();
-        ds.team_number = team;
-        return ds;
+
+        ds.team_number = team.into();
+
+        ds
     }
 
-    pub fn init(mut self) -> Self {
-        self.socket = UdpSocket::bind(
+    pub async fn init(&mut self) {
+        self.socket = UdpSocket::bind::<(Ipv4Addr, u16)>((self.team_number.into(), 3000u16))
+            .await
+            .ok();
 
+        self.connection = Some(
+            tokio::spawn(async {
+                let mut update_rio: Interval = time::interval(Duration::from_millis(20));
 
-        self.connection = Some(tokio::spawn(async {
-            let mut update_rio: Interval = time::interval(Duration::from_millis(20));
-            
-
-            loop {
-                DriverStation::ds_to_rio().await;
-                update_rio.tick().await;
-            }
-        }).abort_handle());
-
-        return self;
-    }
-    
-    pub fn quit(self) -> () {
-        self.connection.unwrap().abort();
+                loop {
+                    DriverStation::ds_to_rio().await;
+                    update_rio.tick().await;
+                }
+            })
+            .abort_handle(),
+        );
     }
 
-    async fn ds_to_rio() {
-
+    pub fn quit(self) {
+        if let Some(connection) = self.connection {
+            // if the connection is none we can assume is was never made
+            connection.abort();
+        }
     }
 
-    fn ds_to_fms() {
+    async fn ds_to_rio() {}
 
+    fn ds_to_fms() {}
+}
+
+impl From<TeamNumber> for Ipv4Addr {
+    fn from(value: TeamNumber) -> Self {
+        let last_digits = ((value.0 as f64) / 100.0).floor();
+
+        Ipv4Addr::new(
+            10,
+            last_digits as u8,
+            ((value.0 as f64) - last_digits * 100.0) as u8,
+            2,
+        )
     }
 }
 
-// NOTE: Produces ip addresses with leading zeros. This should NOT be a problem according to the rust docs. https://doc.rust-lang.org/std/net/struct.Ipv4Addr.html#textual-representation
-pub fn team_number_to_ip(team_number: u16) -> String {
-    let mut tn = team_number.to_string();
-    tn = "0".repeat(4 - tn.len()) + &tn;
-    return "10.".to_owned() + &tn[0..2] + "." + &tn[2..4] + ".2";
+impl From<u16> for TeamNumber {
+    fn from(value: u16) -> Self {
+        Self::new(value)
+    }
 }
-
